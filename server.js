186f0a53194a2dd40a269e11d087c092ed73b50a
Morsby <sigurd.morsby@gmail.com>
2018-06-04 14:17:37 +0200

    Heroku fix v3

--- server.js
+++ server.js
@@ -34,6 +34,21 @@ require('./routes/question')(app);
 // Registrer alle routes fra denne fil (prefixed '/api')
 //app.use('/api', router);
 
+if (process.env.NODE_ENV === 'production') {
+	// Express will serve prod. assets
+	// (js, css files)
+	app.use(express.static('client/build'));
+
+	// Express will serve up index.html if
+	// unknown route
+	// Catchall case, only happens if all above
+	// failed
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 // START SERVEREN
 app.listen(port);
 console.log('Magic happens on port ' + port);
