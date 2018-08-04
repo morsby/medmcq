25fac88bf4f36d1028b4cb7056e3f6e290e03332
Morsby <sigurd.morsby@gmail.com>
2018-08-04 12:07:00 +0200

    Andet feedback commit

---
+++ client/src/components/FeedbackCommentPost.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+import { Container, Button } from 'semantic-ui-react';
+import { Form, Field } from 'react-final-form';
+
+import FeedbackNavigation from './FeedbackNavigation';
+import FeedbackSingleContent from './FeedbackSingleContent';
+import Footer from './Footer';
+
+let initialState = {
+	title: '',
+	text: '',
+	errors: {
+		title: '',
+		text: ''
+	},
+	titleValid: false,
+	textValid: false,
+	formValid: false,
+	displayErr: false
+};
+
+// TODO: Scroll til formular når der klikkes på "svar"
+
+class FeedbackCommentPost extends Component {
+	constructor(props) {
+		super(props);
+		this.state = initialState;
+	}
+
+	handleSubmit = formValues => {
+		formValues.feedbackId = this.props.feedbackId;
+		formValues.parent_id = this.props.replyId;
+		return this.props.postFeedbackComment(formValues);
+	};
+
+	render() {
+		return (
+			<div>
+				<Form
+					onSubmit={this.handleSubmit}
+					validate={values => {
+						const errors = {};
+
+						if (!values.text || values.text.length < 10) {
+							errors.text = 'Teksten skal minimum være 10 tegn';
+						}
+						return errors;
+					}}
+					render={({
+						handleSubmit,
+						submitting,
+						pristine,
+						values,
+						form
+					}) => (
+						<form
+							onSubmit={event => {
+								handleSubmit(event).then(() => form.reset());
+							}}
+							className="ui form"
+						>
+							<Field name="text">
+								{({ input, meta }) => (
+									<div
+										className={
+											'field ' +
+											(meta.error && meta.touched
+												? 'error'
+												: '')
+										}
+									>
+										<label>Kommentar</label>
+										{this.props.replyId && (
+											<p>
+												Du er ved at svare på
+												kommentaren med id:{' '}
+												{this.props.replySlug}
+											</p>
+										)}
+										<div className="ui info message mini form-explanation">
+											<a
+												href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet"
+												target="_blank"
+												rel="noopener noreferrer"
+											>
+												Markdown-formattering
+											</a>{' '}
+											er undersøttet
+										</div>
+										<textarea
+											{...input}
+											placeholder="Forslag"
+										/>
+
+										<div className="form-error">
+											{meta.error &&
+												meta.touched && (
+													<span>{meta.error}</span>
+												)}
+										</div>
+									</div>
+								)}
+							</Field>
+
+							<Button type="submit" disabled={submitting}>
+								Submit
+							</Button>
+						</form>
+					)}
+				/>
+			</div>
+		);
+	}
+}
+
+export default connect(
+	null,
+	actions
+)(FeedbackCommentPost);
---
+++ client/src/components/FeedbackSingleContent.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import marked from 'marked';
+import { Card, Button, Icon, Label } from 'semantic-ui-react';
+
+const FeedbackSingleContent = props => {
+	let votedUp = props.votedFor && props.votedFor.val === 1,
+		votedDown = props.votedFor && props.votedFor.val === -1;
+
+	return (
+		<Card fluid>
+			<Card.Content>
+				<Card.Meta>
+					{new Date(props.date).toLocaleString('da-DK')}
+				</Card.Meta>
+				<Card.Header>{props.title}</Card.Header>
+				<p
+					dangerouslySetInnerHTML={{
+						__html: marked(props.text)
+					}}
+				/>
+			</Card.Content>
+			{props.id && (
+				<Card.Content extra>
+					<Button as="div" labelPosition="right">
+						<Button color="red">
+							<Icon name="heart" />
+						</Button>
+						<Label as="a" basic color="red" pointing="left">
+							{props.votes}
+						</Label>
+					</Button>
+
+					<Button
+						basic
+						positive
+						onClick={() => {
+							props.handleVote(1);
+						}}
+						disabled={votedDown}
+					>
+						👍
+					</Button>
+					<Button
+						basic
+						negative
+						onClick={() => {
+							props.handleVote(-1);
+						}}
+						disabled={votedUp}
+					>
+						👎
+					</Button>
+				</Card.Content>
+			)}
+		</Card>
+	);
+};
+
+export default FeedbackSingleContent;
