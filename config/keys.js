da8af90d86a54e39065ff2f347cb295c4e5f1326
Morsby <sigurd.morsby@gmail.com>
2018-09-20 20:25:48 +0200

    keys

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,11 @@
 // Return correct credentials
-if (process.env.NODE_ENV === 'production') {
-	// we're in production, return these keys
-	module.exports = require('./prod');
+if (
+    process.env.NODE_ENV === "production" ||
+    process.env.NODE_ENV === "au_server"
+) {
+    // we're in production, return these keys
+    module.exports = require("./prod");
 } else {
-	// we're in development, return these keys
-	module.exports = require('./dev');
+    // we're in development, return these keys
+    module.exports = require("./dev");
 }
