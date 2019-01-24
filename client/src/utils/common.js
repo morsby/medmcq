81e49709e83412a1799644effe79a98f6a510429
Morsby <sigurd.morsby@gmail.com>
2019-01-24 17:20:26 +0100

    Krav til brugernavne

--- client/src/utils/common.js
+++ client/src/utils/common.js
@@ -106,5 +106,6 @@ export const allowedNs = {
 };
 
 export const validationRegex = {
+    // username validation stammer fra https://stackoverflow.com/questions/12018245/regular-expression-to-validate-username
     username: /^[a-zA-ZæøåÆØÅ0-9]+([._]?[a-zA-Z0-9]+)*$/,
 };
