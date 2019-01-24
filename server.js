bf82fc3d0eaf1f54fd1fdf8c5c6155a56ec8402f
Morsby <sigurd.morsby@gmail.com>
2019-01-24 16:19:59 +0100

    Flyttede require dotenv

--- server.js
+++ server.js
@@ -1,59 +1,57 @@
+require('dotenv').config();
+
 // server.js
-const helmet = require('helmet')
-const comment = require('./routes/comment')
+const helmet = require('helmet');
+const comment = require('./routes/comment');
 
 // BASE SETUP
-require("dotenv").config();
-const keys = require("./config/keys");
+const keys = require('./config/keys');
 
 // call the packages we need
-var sslRedirect = require("heroku-ssl-redirect");
-var express = require("express");
+var sslRedirect = require('heroku-ssl-redirect');
+var express = require('express');
 var app = express();
 
 // Mandatory Middleware for secure HTTP headers
-app.use(helmet())
+app.use(helmet());
 
 // if heroku, force SSL
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
     app.use(sslRedirect());
 }
 
 // Database
-const mongoose = require("mongoose");
-mongoose.set("useCreateIndex", true);
-mongoose.connect(
-    keys.mongoURI,
-    { useNewUrlParser: true }
-);
+const mongoose = require('mongoose');
+mongoose.set('useCreateIndex', true);
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 // For logins:
-var passport = require("passport");
-var cookieParser = require("cookie-parser");
-var session = require("cookie-session");
+var passport = require('passport');
+var cookieParser = require('cookie-parser');
+var session = require('cookie-session');
 
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(
     session({
         secret: keys.session,
         resave: true,
-        saveUninitialized: true
+        saveUninitialized: true,
     })
 ); // session secret
 
-require("./config/passport")(passport); // pass passport for configuration
+require('./config/passport')(passport); // pass passport for configuration
 
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 
 // bodyParser tillader JSON-posts
-var bodyParser = require("body-parser");
+var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // multer tillader filuploads (billeder til spørgsmål)
-const multer = require("multer");
-const cloudinary = require("cloudinary");
+const multer = require('multer');
+const cloudinary = require('cloudinary');
 
 var port = process.env.PORT || 3001; // set our port
 
@@ -61,37 +59,37 @@ var port = process.env.PORT || 3001; // set our port
 var router = express.Router();
 
 // log requests to DB
-const saveReq = require("./middleware/saveReq");
+const saveReq = require('./middleware/saveReq');
 
 app.use(saveReq);
 
 // more routes for our API will happen here
-require("./routes/question")(app);
-require("./routes/feedback")(app);
-app.use("./routes/comment", comment);
-require("./routes/user")(app);
+require('./routes/question')(app);
+require('./routes/feedback')(app);
+app.use('./routes/comment', comment);
+require('./routes/user')(app);
 
 // Registrer alle routes fra denne fil (prefixed '/api')
 //app.use('/api', router);
 
 if (
-    process.env.NODE_ENV === "production" ||
-    process.env.NODE_ENV === "au_server"
+    process.env.NODE_ENV === 'production' ||
+    process.env.NODE_ENV === 'au_server'
 ) {
     // Express will serve prod. assets
     // (js, css files)
-    app.use(express.static("client/build"));
+    app.use(express.static('client/build'));
 }
 
 // Express will serve up index.html if
 // unknown route
 // Catchall case, only happens if all above
 // failed
-const path = require("path");
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+const path = require('path');
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 // START SERVEREN
 app.listen(port);
-console.log("Magic happens on port " + port);
+console.log('Magic happens on port ' + port);
