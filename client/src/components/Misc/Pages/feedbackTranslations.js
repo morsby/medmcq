e874e65a898ade40bd806ccb27b32bb09c77e2b0
thomasjensen1194 <44089656+thomasjensen1194@users.noreply.github.com>
2019-02-21 11:33:20 +0100

    Ændrede kontakt information
    - Både engelsk og dansk

--- client/src/components/Misc/Pages/feedbackTranslations.js
+++ client/src/components/Misc/Pages/feedbackTranslations.js
@@ -7,28 +7,32 @@ export default {
 
 Så vil vi meget gerne høre fra dig på mailadressen medmcq@fire.fundersclub.com. Men læs lige videre nedenfor først!
 
-Den mailadresse er lidt speciel – der er ingen mennesker, der læser den – det gør til gengæld en meget samarbejdsvillig bot. Det betyder, at alle emails  sendt til adressen automatisk bliver overført til det system, vi udviklere benytter til at kommunikere og dele sidens kode med hinanden. Det er en måde at sikre på, at *din* feedback aldrig strander hos en færdiguddannet og jeg-har-da-alt-for-travlt-til-det-her læge men altid når frem til dem, der har fingrene i maskineriet.
-
-Al feedback foregår således offentligt på GitHub, og her kan du se både [koden](https://github.com/morsby/au-medicin-mcq) bag hjemmesiden og den [feedback](https://github.com/morsby/au-medicin-mcq/issues) vi tidligere har modtaget.
-
 Hvis du har fundet en **fejl**, vil vi gerne have, at du er så **specifik** som mulig i din beskrivelse. Hvad gjorde du, hvad skete der, og hvad havde du forventet? Og er der tale om en fejl i et eksamensspørgsmål, vil vi meget gerne have et screenshot af *hele* spørgsmålet med i mailen (inkl. hvilket sæt det er fra).
 
-Hvis du sender os en mail på adressen, modtager du et svar fra vores bot. Han foreslår, at du trykker på knappen "Link your GitHub". Det behøver du *overhovedet* ikke. Hvis du vil følge med (at deltage kræver en gratis GitHub-bruger) i samtalen omkring din mail, kan du blot besøge siden med [feedback](https://github.com/morsby/au-medicin-mcq/issues). 
+Mailadressen er lidt speciel. Den varetages af en hårdarbejdende computer, som videresender din mail til sidens udviklere. Det er en måde at sikre på, at din feedback aldrig strander hos en færdiguddannet og jeg-har-da-alt-for-travlt-til-det-her læge men altid når frem til dem, der har fingrene i maskineriet.
+
+Al feedback foregår således offentligt på GitHub, og her kan du se både [koden](https://github.com/morsby/au-medicin-mcq) bag hjemmesiden og den [feedback](https://github.com/morsby/au-medicin-mcq/issues) vi tidligere har modtaget. Dit navn vil også stå offentligt, hvis dit mail-program er indstillet til at sende dit navn med emailen (hvis ikke, er dit feedback anonymt).
+
+Hvis du sender os en mail på adressen, modtager du et svar fra vores bot. Han foreslår, at du trykker på knappen "Link your GitHub". Det behøver du *overhovedet* ikke. Hvis du vil følge med i samtalen omkring din mail, kan du blot besøge siden med [feedback](https://github.com/morsby/au-medicin-mcq/issues).
+
+Hvis du gerne vil modtage notifikationer direkte til din mail inbox, så kræver det at du opretter en gratis GitHub-bruger.
 
 Tak for beskeden!
 `,
             `
-**Do you have suggestions for improvements, new additions, or have you discovered a bug on this site or an exam set?**
+**Do you have suggestions for improvements, new additions, or have you discovered a bug on this site or in an exam set?**
 
-Then we'd love to hear from you at the e-mail address medmcq@fire.fundersclub.com. But please – read on first!
+Then we would love to hear from you at the e-mail address medmcq@fire.fundersclub.com. But please – read on first!
 
-That e-mail address is a little special – no humans ever read it – but a cooperative bot does! This means that any email sent to the address is automatically transferred to the system used by us developers to communicate and share code amongst us. This is a way to ensure that *your* feedback never lands in the inbox of a graduated and I'm-way-too-busy-for-this doctor and always reaches the people currently maintaining the site.
+If you have uncovered a **bug**, we would like you to be as **specific** as possible when describing it. What did you do, what happened, and what did you expect to happen? If it is a problem with a specific question, please attach a screenshot of the *entire* question (including which exam set it is from) in the email. 
 
-As such, all feedback is pulic on GitHub. Here you can see both the [code](https://github.com/morsby/au-medicin-mcq) for the site as well as all previous [feedback](https://github.com/morsby/au-medicin-mcq/issues).
+The email-address is a little special. It is managed by a hardworking computer, which forwards your mail to the developers of the site. This is a way to ensure that your feedback never ends up in the inbox of a graduated and I'm-way-too-busy-for-this doctor and always reaches the people currently maintaining the site.
 
-If you have uncovered a **bug**, we'd like you to be as **specific** as possible when describing it. What did you do, what happened, and what had you expected to happen? If it's a problem with a specific question, please attach a screenshot of the *entire* question (including which exam set it's from) in the email. 
+As such, all feedback is public on GitHub. Here you can see both the [code](https://github.com/morsby/au-medicin-mcq) for the site, as well as all previous [feedback](https://github.com/morsby/au-medicin-mcq/issues). Your name will also be public, if your mail-software is configured to send your name alongside your emails (if it is not, then your feedback is anonymous).
 
-If you send us a mail at the above address, you will receieve a reply from our bot. He suggests you "Link your Github". However, there is *absolutely* no need to do this. If you wish to follow the conversation about your email (participating in it requires a GitHub account), just visit the [feedback](https://github.com/morsby/au-medicin-mcq/issues) page.
+If you send us a mail at the above address, you will receieve a reply from our computer. He suggests you "Link your Github". However, there is *absolutely* no need to do this. If you wish to follow the conversation about your email just visit the [feedback](https://github.com/morsby/au-medicin-mcq/issues) page.
+
+If you wish to recieve a notification to your email inbox, everytime there is an update on your issue, it is required to create a free GitHub account. 
 
 Thank you for your message!
 `
