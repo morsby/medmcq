5f2aebb4d0014ff8a2085e6f3f5fd6707b65e599
Thomas <thomasjensen1194@gmail.com>
2019-04-22 09:21:37 +0200

    Tilføjede Grid til speciale/tags vælger
    På mobilen er det stackable, så det også kan ses. Det giver et bedre overblik og bedre brug af siden på store skærme.

--- client/src/components/SelectionSettings/SelectionSettings/SelectionSpecialtiesSelector/SelectionSpecialtiesSelector.js
+++ client/src/components/SelectionSettings/SelectionSettings/SelectionSpecialtiesSelector/SelectionSpecialtiesSelector.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { specialer, tags } from '../../../../utils/common';
 import { Translate } from 'react-localize-redux';
 
-import { Form, Header, Message } from 'semantic-ui-react';
+import { Form, Header, Message, Grid } from 'semantic-ui-react';
 import SelectionSpecialtiesSelectorCheckbox from './SelectionSpecialtiesSelectorCheckbox';
 
 /**
@@ -26,41 +26,48 @@ const SelectionSpecialtiesSelector = ({
     );
   return (
     <Form>
-      <Header as="h3">
-        <Translate id="selectionSpecialtiesSelector.header" data={{ semester }} />
-      </Header>
-
-      {specialer[semester].map((speciale) => {
-        let erValgt = valgteSpecialer.includes(speciale.value);
-        return (
-          <SelectionSpecialtiesSelectorCheckbox
-            key={speciale.value}
-            type="specialer"
-            speciale={speciale}
-            erValgt={erValgt}
-            antalPerSpeciale={antalPerSpeciale[speciale.value]}
-            onChange={onChange}
-          />
-        );
-      })}
-
-      <Header as="h3">
-        <Translate id="selectionSpecialtiesSelector.tags" data={{ semester }} />
-      </Header>
-
-      {tags[semester].map((tag) => {
-        let erValgt = valgteTags.includes(tag.value);
-        return (
-          <SelectionSpecialtiesSelectorCheckbox
-            key={tag.value}
-            type="tags"
-            speciale={tag}
-            erValgt={erValgt}
-            antalPerSpeciale={antalPerTag[tag.value]}
-            onChange={onChange}
-          />
-        );
-      })}
+      <Grid columns="equal" stackable>
+        <Grid.Column>
+          <Grid.Row>
+            <Header as="h3">
+              <Translate id="selectionSpecialtiesSelector.header" data={{ semester }} />
+            </Header>
+            {specialer[semester].map((speciale) => {
+              let erValgt = valgteSpecialer.includes(speciale.value);
+              return (
+                <SelectionSpecialtiesSelectorCheckbox
+                  key={speciale.value}
+                  type="specialer"
+                  speciale={speciale}
+                  erValgt={erValgt}
+                  antalPerSpeciale={antalPerSpeciale[speciale.value]}
+                  onChange={onChange}
+                />
+              );
+            })}
+          </Grid.Row>
+        </Grid.Column>
+        <Grid.Column>
+          <Grid.Row>
+            <Header as="h3">
+              <Translate id="selectionSpecialtiesSelector.tags" data={{ semester }} />
+            </Header>
+            {tags[semester].map((tag) => {
+              let erValgt = valgteTags.includes(tag.value);
+              return (
+                <SelectionSpecialtiesSelectorCheckbox
+                  key={tag.value}
+                  type="tags"
+                  speciale={tag}
+                  erValgt={erValgt}
+                  antalPerSpeciale={antalPerTag[tag.value]}
+                  onChange={onChange}
+                />
+              );
+            })}
+          </Grid.Row>
+        </Grid.Column>
+      </Grid>
 
       <Message info>
         <Translate id="selectionSpecialtiesSelector.tags_explanation" />
