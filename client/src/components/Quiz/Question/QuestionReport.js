507b0275f4195bd1ef0dcb8ade7aca000f4b144b
Thomas Jensen <44089656+thomasjensen1194@users.noreply.github.com>
2019-05-03 13:28:05 +0200

    Added check for issues on question
    - Man kan nu direkte for spørgsmålet tjekke om der er andre issues under samme spørgsmål, inden man rapporterer en fejl.

--- client/src/components/Quiz/Question/QuestionReport.js
+++ client/src/components/Quiz/Question/QuestionReport.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Form, Message } from 'semantic-ui-react';
 import { Translate } from 'react-localize-redux';
 
 // TODO: TRANSLATION
 const QuestionReport = ({ report, handleChange, handleSubmit, reportSent, question }) => {
+  const [checked, setChecked] = useState(false);
   if (reportSent)
     return (
       <Message success>
@@ -18,13 +19,16 @@ const QuestionReport = ({ report, handleChange, handleSubmit, reportSent, questi
     return (
       <Translate>
         {({ translate }) => (
-          <Form onSubmit={handleSubmit}>
+          <Form onSubmit={checked ? handleSubmit : null}>
             {question.semester === 11 && (
               <Message color="yellow">
                 <Translate id="questionReport.pictureMissing11" />
               </Message>
             )}
             {question.disclaimer && <Message color="yellow">{question.disclaimer}</Message>}
+            <Message>
+              <Translate id="questionReport.checkIssue" data={{ link: question._id }} />
+            </Message>
             <Form.Field>
               <Form.TextArea
                 name="report"
@@ -34,7 +38,19 @@ const QuestionReport = ({ report, handleChange, handleSubmit, reportSent, questi
                 value={report}
               />
             </Form.Field>
-            <Form.Button color="orange">Send</Form.Button>
+            <Form.Field>
+              <Form.Checkbox
+                required
+                checked={checked}
+                onChange={() => setChecked(!checked)}
+                label={
+                  <label>
+                    <Translate id="questionReport.checkbox" data={{ link: question._id }} />
+                  </label>
+                }
+              />
+              <Form.Button color="orange">Send</Form.Button>
+            </Form.Field>
           </Form>
         )}
       </Translate>
