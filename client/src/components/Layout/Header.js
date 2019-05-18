fcd106ff131f69f4c3eca1497cdcfcf6ef58f48a
Morsby <sigurd.morsby@gmail.com>
2019-05-18 10:13:18 +0200

    Header does fetchUser every min

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
 import { Translate } from 'react-localize-redux';
 
 import { Menu, Responsive } from 'semantic-ui-react';
@@ -13,6 +15,8 @@ import LeftMenu from './Menus/LeftMenu';
  * Header-component. Viser headeren og tjekker at brugeren er logget ind.
  */
 const Header = (props) => {
+  props.fetchUser();
+  setInterval(props.fetchUser, 60 * 1000);
   return (
     <Responsive as="header" minWidth={breakpoints.mobile}>
       <h2 className={styles.onprint}>
@@ -28,4 +32,7 @@ const Header = (props) => {
   );
 };
 
-export default Header;
+export default connect(
+  null,
+  actions
+)(Header);
