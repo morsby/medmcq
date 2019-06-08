c4c83ff527c44c24150d3f1bfaffab04815f8bf3
Thomas Jensen <thomasjensen1194@gmail.com>
2019-06-08 21:05:11 +0200

    Integrated new dropdown from antdesign

--- client/src/components/Quiz/Question/QuestionMetadata/QuestionMetadataDropdown.jsx
+++ client/src/components/Quiz/Question/QuestionMetadata/QuestionMetadataDropdown.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Dropdown } from 'semantic-ui-react';
 import _ from 'lodash';
+import Dropdown from 'antd/lib/dropdown';
+import Menu from 'antd/lib/menu';
+import 'antd/dist/antd.css';
+import { Icon } from 'semantic-ui-react';
+const { SubMenu } = Menu;
 
 const QuestionMetadataDropdown = ({ options, onChange, text, type }) => {
-  const handleDropdownPick = (e, { value }) => {
-    onChange(value);
+  const handleDropdownPick = ({ key }) => {
+    onChange(key);
   };
 
   const createDropdownItems = (tags) => {
@@ -12,12 +16,14 @@ const QuestionMetadataDropdown = ({ options, onChange, text, type }) => {
 
     for (const t of tags) {
       items.push(
-        <Dropdown.Item onClick={handleDropdownPick} text={t.text} value={t.value} key={t.value} />
+        <Menu.Item onClick={handleDropdownPick} key={t.value}>
+          {t.text}
+        </Menu.Item>
       );
     }
 
     if (items.length === 0) {
-      items.push(<Dropdown.Item text="Ingen tags..." />);
+      items.push(<Menu.Item text="Ingen tags..." />);
     }
 
     return items;
@@ -31,11 +37,9 @@ const QuestionMetadataDropdown = ({ options, onChange, text, type }) => {
 
       for (const key in groupedTags) {
         dropdown.push(
-          <Dropdown.Item key={key}>
-            <Dropdown scrolling text={key.charAt(0).toUpperCase() + key.slice(1)}>
-              <Dropdown.Menu>{createDropdownItems(groupedTags[key])}</Dropdown.Menu>
-            </Dropdown>
-          </Dropdown.Item>
+          <SubMenu title={key.charAt(0).toUpperCase() + key.slice(1)}>
+            {createDropdownItems(groupedTags[key])}
+          </SubMenu>
         );
       }
 
@@ -45,16 +49,34 @@ const QuestionMetadataDropdown = ({ options, onChange, text, type }) => {
     if (type === 'specialty') {
       return options.map((o) => {
         return (
-          <Dropdown.Item onClick={handleDropdownPick} key={o.value} text={o.text} value={o.value} />
+          <Menu.Item onClick={handleDropdownPick} key={o.value}>
+            {o.text}
+          </Menu.Item>
         );
       });
     }
   };
 
   return (
-    <span style={{ margin: '5px', border: '1px solid lightgrey', padding: '5px' }}>
-      <Dropdown text={text}>
-        <Dropdown.Menu>{createDropdowns()}</Dropdown.Menu>
+    <span
+      style={{
+        margin: '5px',
+        padding: '6px',
+        backgroundColor: '#ededed',
+        borderRadius: '5px',
+        color: '#727272',
+        whiteSpace: 'nowrap',
+        display: 'inline-block',
+        fontSize: '12px',
+        fontWeight: '700'
+      }}
+    >
+      <Dropdown overlay={<Menu>{createDropdowns()}</Menu>}>
+        <span>
+          <Icon name="plus" />
+          Add {type}
+          <Icon name="dropdown" />
+        </span>
       </Dropdown>
     </span>
   );
