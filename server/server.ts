593584ccec0017a83f95c330a8caf99145475768
Thomas Jensen <thomasjensen1194@gmail.com>
2019-07-02 21:00:20 +0200

    Implemented typescript with tests

--- server/server.ts
+++ server/server.ts
@@ -54,8 +54,10 @@ app.use('/api', routes);
 
 /* Catch all */
 const path = require('path');
+
+app.use(express.static(path.join(__dirname, "..", '..', 'client', 'build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '..', '..', 'client', 'build', 'index.html'));
 });
 
 // START SERVEREN
