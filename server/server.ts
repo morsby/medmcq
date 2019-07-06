96094c8fe53259bd73a73087dc7a403df3275903
Thomas Jensen <thomasjensen1194@gmail.com>
2019-07-06 17:06:55 +0300

    Fixed nodemon + semantic update

--- server/server.ts
+++ server/server.ts
@@ -55,7 +55,7 @@ app.use('/api', routes);
 /* Catch all */
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, "..", '..', 'client', 'build')));
+app.use(express.static(path.join(__dirname, '..', '..', 'client', 'build')));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', '..', 'client', 'build', 'index.html'));
 });
