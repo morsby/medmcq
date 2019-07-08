81212f0b9fbe34daee3d6f9b6628394955c1fb73
Morsby <sigurd.morsby@gmail.com>
2019-07-08 11:44:26 +0200

    Fix failing tests

--- server/migrations/20190623195242_tags_oldId_nesting.js
+++ server/migrations/20190623195242_tags_oldId_nesting.js
@@ -1,18 +1,18 @@
-exports.up = function(knex, Promise) {
-  return knex.schema.table("question_tag", function(t) {
-    t.string("old_id");
-    t.integer("parent_id")
+exports.up = function(knex) {
+  return knex.schema.table('question_tag', function(t) {
+    t.string('old_id');
+    t.integer('parent_id')
       .unsigned()
-      .references("id")
-      .inTable("question_tag")
-      .onDelete("SET NULL");
+      .references('id')
+      .inTable('question_tag')
+      .onDelete('SET NULL');
   });
 };
 
-exports.down = function(knex, Promise) {
-  return knex.schema.table("question_tag", function(t) {
-    t.dropColumn("old_id");
-    t.dropForeign("parent_id");
-    t.dropColumn("parent_id");
+exports.down = function(knex) {
+  return knex.schema.table('question_tag', function(t) {
+    t.dropColumn('old_id');
+    t.dropForeign('parent_id');
+    t.dropColumn('parent_id');
   });
 };
