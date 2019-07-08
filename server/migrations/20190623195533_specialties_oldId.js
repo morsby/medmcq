7ffba9e526b3a93d95dc51649d1fb8436f0c7fae
Morsby <sigurd.morsby@gmail.com>
2019-07-08 11:44:26 +0200

    Fix failing tests

--- server/migrations/20190623195533_specialties_oldId.js
+++ server/migrations/20190623195533_specialties_oldId.js
@@ -1,11 +1,11 @@
-exports.up = function(knex, Promise) {
-  return knex.schema.table("question_specialty", function(t) {
-    t.string("old_id");
+exports.up = function(knex) {
+  return knex.schema.table('question_specialty', function(t) {
+    t.string('old_id');
   });
 };
 
-exports.down = function(knex, Promise) {
-  return knex.schema.table("question_specialty", function(t) {
-    t.dropColumn("old_id");
+exports.down = function(knex) {
+  return knex.schema.table('question_specialty', function(t) {
+    t.dropColumn('old_id');
   });
 };
