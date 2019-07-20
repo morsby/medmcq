a14bfb7fe2e2d2134d513f4b1b934f0893f67cda
Thomas Jensen <44089656+thomasjensen1194@users.noreply.github.com>
2019-07-20 21:15:45 +0200

    Added tags and specialties selection tree (#364)
    * Added tags and specialties selection tree

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,5 +1,3 @@
-import { insertOrRemoveFromArray } from './common';
-
 // UI REDUCER
 export const updateSelection = (prevSelection, action) => {
   let { type, value } = action.payload;
@@ -9,11 +7,6 @@ export const updateSelection = (prevSelection, action) => {
     selection.selectedSetId = null;
     selection.selectedSpecialtyIds = [];
     selection.selectedTagIds = [];
-  }
-  // Hvis vi ændrer specialer/tags, opdaterer vi array'et
-  if (['selectedSpecialtyIds', 'selectedTagIds'].indexOf(type) > -1) {
-    let currIds = Array.isArray(prevSelection[type]) ? prevSelection[type] : [];
-    value = insertOrRemoveFromArray(currIds, value);
   }
   selection[type] = value;
 
