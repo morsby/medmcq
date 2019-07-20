038be5a5ef3b660b690537b888b445e42f36f559
Thomas Jensen <44089656+thomasjensen1194@users.noreply.github.com>
2019-07-20 21:15:19 +0200

    Manual complete sets (#363)
    * Can complete sets manually
    
    - Deleted leftover (redundant) files
    - Added completedSets migration
    - Added manualCompletedSets to migrations and user object
    - TODO: Auto-calculation of completed sets by answers

---
+++ server/migrations/20190720151525_create_manual_completed_set.js
@@ -0,0 +1,22 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('manual_completed_sets', (t) => {
+    t.increments();
+    t.integer('user_id')
+      .unsigned()
+      .notNullable()
+      .references('user.id')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+    t.integer('set_id')
+      .unsigned()
+      .notNullable()
+      .references('semester_exam_set.id')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+    t.timestamps();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('manual_completed_sets');
+};
