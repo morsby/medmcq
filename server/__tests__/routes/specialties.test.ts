c9b74d5ffc5eb9130baa91cd7f8ac5337ecfdce4
Thomas Jensen <thomasjensen1194@gmail.com>
2019-07-21 13:46:53 +0200

    Specialties test working

--- server/__tests__/routes/specialties.test.ts
+++ server/__tests__/routes/specialties.test.ts
@@ -1,33 +1,44 @@
 import _ from 'lodash';
 import request from 'supertest';
+import sample_specialties from './data/sample_specialties';
+import Specialty from '../../models/specialty';
+import { createUsers, createSemesters } from './functions/creation';
+import User from '../../models/user';
+import UserRole from '../../models/user_role';
+import Semester from '../../models/semester';
+const authApi = '/api/auth';
 const specialtyApi = '/api/specialties';
 let server;
-let agent;
+let admin;
 
 describe('specialties route', () => {
-  beforeEach(() => {
+  beforeEach(async () => {
     server = require('../../server');
-    agent = request.agent(server);
+    admin = request.agent(server);
+
+    await createSemesters();
+    await createUsers();
+    await Specialty.query().insertGraph(sample_specialties);
+
+    // Login
+    const res = await admin.post(authApi).send({ username: 'admin', password: '123abc' });
+    expect(res.body.type).toEqual('LoginSuccess');
   });
 
-  afterEach(() => {
+  afterEach(async () => {
+    await Semester.query().delete();
+    await Specialty.query().delete();
+    await User.query().delete();
+    await UserRole.query().delete();
     server.close();
   });
-
-  // Settings vars to reuse across tests
-  let firstSpecialtyId, newSpecialtyId;
 
   test("GET '/' -- should get all specialties", async () => {
     let response = await request(server).get(specialtyApi);
     let specialties = response.body;
 
-    // To reuse the id without making a second API call -- id can change if rerunning seeds
-    firstSpecialtyId = specialties[0].id;
-    let sortedSpecialties = _.sortBy(specialties, ['name']);
-    let seededSpecialties = require('../../seeds/data/12_sample_specialer.json');
-    seededSpecialties = seededSpecialties.map((s) => s.name).sort();
-
-    expect(sortedSpecialties.map((sem) => sem.name)).toEqual(seededSpecialties);
+    expect(specialties.length).toBe(5);
+    expect(typeof specialties[0].name).toBe('string');
   });
 
   test("POST '/' -- should fail because not admin", async () => {
@@ -43,19 +54,16 @@ describe('specialties route', () => {
   });
 
   test("POST '/' -- should insert a new specialty", async () => {
-    await agent.post('/api/auth').send({ username: 'TestAdmin', password: 'TestPassword123' });
-
-    let { body } = await agent.post(specialtyApi).send({
+    let { body } = await admin.post(specialtyApi).send({
       name: 'Test',
       semesterId: 4
     });
-    newSpecialtyId = body.id;
     expect(body.semesterId).toEqual(4);
     expect(body.name).toEqual('Test');
   });
 
   test("POST '/' -- should fail with missing props", async () => {
-    let { status, body } = await agent.post(specialtyApi).send({
+    let { status, body } = await admin.post(specialtyApi).send({
       name: 'Should fail'
     });
 
@@ -64,10 +72,10 @@ describe('specialties route', () => {
   });
 
   test("GET '/:id' -- should get one specialty", async () => {
-    let { body } = await request(server).get(`${specialtyApi}/${firstSpecialtyId}`);
+    let { body } = await request(server).get(`${specialtyApi}/1`);
 
-    expect(body.name).toEqual('Almen medicin');
     expect(body.semesterId).toEqual(1);
+    expect(typeof body.name).toBe('string');
     expect(body).toHaveProperty('semester');
     expect(body).toHaveProperty('voteCount');
     expect(Array.isArray(body.questions)).toBe(true);
@@ -75,7 +83,7 @@ describe('specialties route', () => {
 
   test("PATCH '/:id' -- should fail because not admin", async () => {
     let { status, body } = await request(server)
-      .patch(`${specialtyApi}/${newSpecialtyId}`)
+      .patch(`${specialtyApi}/1`)
       .send({
         name: 'NewName'
       });
@@ -84,20 +92,20 @@ describe('specialties route', () => {
   });
 
   test("PATCH '/:id' -- should patch a specialty", async () => {
-    let { body } = await agent.patch(`${specialtyApi}/${newSpecialtyId}`).send({
+    let { body } = await admin.patch(`${specialtyApi}/1`).send({
       name: 'NewName'
     });
     expect(body.name).toEqual('NewName');
   });
 
   test("DELETE '/:id' -- should fail because not admin", async () => {
-    let { body, status } = await request(server).delete(`${specialtyApi}/${newSpecialtyId}`);
+    let { body, status } = await request(server).delete(`${specialtyApi}/1`);
     expect(status).toEqual(403);
     expect(body.type).toEqual('NotAuthorized');
   });
 
   test("DELETE '/:id' -- should delete a specialty", async () => {
-    let { body } = await agent.delete(`${specialtyApi}/${newSpecialtyId}`);
+    let { body } = await admin.delete(`${specialtyApi}/1`);
     expect(body.type).toEqual('deleteSpecialty');
   });
 });
