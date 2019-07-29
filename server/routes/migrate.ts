b013c970a6079e8a7e2d5385e7ad474915ba54bd
Thomas Jensen <thomasjensen1194@gmail.com>
2019-07-29 18:31:56 +0200

    Updated migration api

--- server/routes/migrate.ts
+++ server/routes/migrate.ts
@@ -2,12 +2,13 @@ import express from 'express';
 const router = express.Router();
 import { knex } from '../server';
 const { migrationPass } = require('../config/keys');
+const directory = process.env.NODE_ENV === 'production' ? 'server/migrations' : './migrations';
 
 router.get('/latest', async (req, res) => {
   const { pass } = req.query;
   if (pass !== migrationPass) return res.status(403).send('Not allowed');
 
-  const result = await knex.migrate.latest();
+  const result = await knex.migrate.latest({ directory });
   res.status(200).send(result);
 });
 
@@ -15,7 +16,7 @@ router.get('/rollback', async (req, res) => {
   const { pass } = req.query;
   if (pass !== migrationPass) return res.status(403).send('Not allowed');
 
-  const result = await knex.migrate.rollback();
+  const result = await knex.migrate.rollback({ directory });
   res.status(200).send(result);
 });
 
