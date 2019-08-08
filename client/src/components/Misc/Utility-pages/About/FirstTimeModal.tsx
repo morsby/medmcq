bb160646bc31582ff3fe7eea4670c06512fa72aa
Thomas Jensen <thomasjensen1194@gmail.com>
2019-08-08 09:10:35 +0200

    Updated some text

--- client/src/components/Misc/Utility-pages/About/FirstTimeModal.tsx
+++ client/src/components/Misc/Utility-pages/About/FirstTimeModal.tsx
@@ -36,27 +36,34 @@ const FirstTimeModal: React.SFC<FirstTimeModalProps> = ({ addTranslation }) => {
     >
       <Modal.Header style={{ textAlign: 'center' }}>Velkommen til medMCQ</Modal.Header>
       <Modal.Content>
-        Kære bruger. Hvis du er ny bruger af siden så anbefaler vi dig at læse følgende. Hvis du er
-        gammel bruger, så kan det også være du finder funktioner, som du ikke kender til.
+        Kære bruger. Velkommen til medMCQ! Hvis du er ny bruger af siden så anbefaler vi dig at læse
+        følgende. Hvis du er gammel bruger, så kan det også være du finder funktioner, som du ikke
+        kender til.
         <br />
         Nogle af sidens funktioner afhænger af, om du opretter en gratis bruger eller ej.
-        <h5>Funktioner uden bruger</h5>
+        <h2>Information</h2>
+        <Translate id="about.frontDisclaimer" />
+        <h2>Funktioner</h2>
+        <h4>Uden bruger</h4>
         <Grid divided="vertically" stackable columns="equal">
           <Grid.Row>
             <Grid.Column verticalAlign="middle">
               <ul>
                 <li>
-                  Valg af spørgsmål tilfældigt, ud fra specialer eller tags eller ud fra hele
+                  <b>Valg af spørgsmål tilfældigt</b>, ud fra specialer eller tags eller ud fra hele
                   eksamenssæt
                 </li>
                 <li>
-                  Besvarelse af 1000-vis af spørgsmål fra tidligere eksamenssæt - helt gratis og
-                  uden begrænsning!
+                  <b>Besvarelse af 1000-vis af spørgsmål</b> fra tidligere eksamenssæt - helt gratis
+                  og uden begrænsning!
                 </li>
-                <li>Dele spørgsmålene med din læsegruppe eller underviser gennem direkte links.</li>
                 <li>
-                  Mulighed for at læse kommentarer under spørgsmålene, blandt de 100-vis af
-                  kommentarer, der er blevet skrevet på siden.
+                  <b>Dele spørgsmål</b> med din læsegruppe eller underviser gennem direkte links.
+                </li>
+                <li>
+                  <b>Læsning af kommentarer</b> under spørgsmålene, blandt de 100-vis af
+                  kommentarer, der er blevet skrevet på siden. Mange kommentarer indeholder gode
+                  forklaringer og tips til spørgsmålene.
                 </li>
               </ul>
             </Grid.Column>
@@ -71,7 +78,7 @@ const FirstTimeModal: React.SFC<FirstTimeModalProps> = ({ addTranslation }) => {
           </Grid.Row>
         </Grid>
         <Divider />
-        <h3>Oprettelse af bruger</h3>
+        <h2>Med bruger</h2>
         <h5>
           Du behøver ikke oprette en gratis bruger, men vær opmærksom på, at du med en bruger får
           flere funktioner:
@@ -79,6 +86,7 @@ const FirstTimeModal: React.SFC<FirstTimeModalProps> = ({ addTranslation }) => {
         <Grid divided="vertically" verticalAlign="middle" columns="equal">
           <Grid.Row>
             <Grid.Column>
+              Du kan
               <ul>
                 <li>
                   <b>Skrive kommentarer:</b> Du vil have mulighed under spørgsmålene at skrive
@@ -88,15 +96,17 @@ const FirstTimeModal: React.SFC<FirstTimeModalProps> = ({ addTranslation }) => {
                   nemmere. Du kan endda skrive kommentarer helt anonymt!
                 </li>
                 <li>
-                  <b>Private kommentarer:</b> Selvom vi anbefaler at skrive offentlige eller anonyme
-                  kommentarer, sådan at alle for gavn af din viden, så har du med en bruger også
-                  mulighed for at skrive private kommentarer, som du kun selv kan se.
+                  Skrive <b>private kommentarer:</b> Selvom vi anbefaler at skrive offentlige eller
+                  anonyme kommentarer, sådan at alle for gavn af din viden, så har du med en bruger
+                  også mulighed for at skrive private kommentarer, som du kun selv kan se.
                 </li>
-                <li>Gemme spørgsmål, så du kan vende tilbage til dem senere</li>
+                <li>
+                  <b>Gemme spørgsmål</b>, så du kan vende tilbage til dem senere
+                </li>
                 <li>Stemme på specialer og tags, for at kategorisere spørgsmålene</li>
                 <li>
-                  Tilgå din profilside med statistik over hvilke spørgsmål du har besvaret, og
-                  hvordan det er gået
+                  <b>Tilgå din profilside</b> med statistik over hvilke spørgsmål du har besvaret,
+                  og hvordan det er gået
                 </li>
               </ul>
             </Grid.Column>
@@ -110,18 +120,14 @@ const FirstTimeModal: React.SFC<FirstTimeModalProps> = ({ addTranslation }) => {
             </Grid.Column>
           </Grid.Row>
         </Grid>
-        <h3>Vigtig information fra dine undervisere</h3>
-        <Message warning>
-          <Translate id="about.frontDisclaimer" />
-        </Message>
-        <Icon name="heartbeat" />
+        <h3>Tak!</h3>
         Du ønskes held og lykke med eksamenslæsningen og god fornøjelse fra os - Sigurd Morsby og
-        Thomas Jensen.
+        Thomas Jensen <Icon name="heartbeat" />
       </Modal.Content>
       <Modal.Actions>
         <div style={{ textAlign: 'center' }}>
-          <Button size="huge" color="green" onClick={handleClose}>
-            Tak for informationen! Tag mig til siden.
+          <Button basic size="huge" color="green" onClick={handleClose}>
+            Begynd!
           </Button>
         </div>
       </Modal.Actions>
