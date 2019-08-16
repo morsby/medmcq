4675f81ae7b58212d2d81e7ddd21f94512be60e7
Thomas Jensen <thomasjensen1194@gmail.com>
2019-08-16 09:11:02 +0200

    Update Sharebuilder.tsx

--- client/src/pages/Sharebuilder.tsx
+++ client/src/pages/Sharebuilder.tsx
@@ -222,7 +222,11 @@ const Sharebuilder: React.SFC<SharebuilderProps> = ({ history }) => {
           />
         </div>
         <Divider hidden />
-        <Button type="primary" disabled={createLinkLoading || picked.length < 1} onClick={handleCreateLink}>
+        <Button
+          type="primary"
+          disabled={createLinkLoading || picked.length < 1}
+          onClick={handleCreateLink}
+        >
           Opret link
         </Button>
         {createLinkData && (
