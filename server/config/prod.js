ea6501439e2e2b183b4973c7c9ee0ccdfe56192f
Thomas Jensen <44089656+thomasjensen1194@users.noreply.github.com>
2019-10-01 14:17:53 +0200

    Updated config

--- server/config/prod.js
+++ server/config/prod.js
@@ -5,7 +5,8 @@ const dbConnection = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_DB
+  database: process.env.DB_DB,
+  port: process.env.DB_PORT || 3306
 };
 
 module.exports = {
