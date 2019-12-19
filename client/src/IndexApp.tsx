e588c87412a906cf53fa2735ce2c73252131032c
Thomas Jensen <44089656+thomasjensen1194@users.noreply.github.com>
2019-12-19 22:10:41 +0100

    Internet explorer disclaimer

---
+++ client/src/IndexApp.tsx
@@ -0,0 +1,79 @@
+/* eslint-disable import/first */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+
+// GraphQL
+import { ApolloProvider } from 'react-apollo-hooks';
+import apolloClient from 'apolloClient';
+// Redux
+import { Provider } from 'react-redux';
+import { createMigrate, persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/es/storage';
+import autoMergeLevel2 from 'redux-persist/es/stateReconciler/autoMergeLevel2';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import reducers, { IReduxState } from './reducers';
+
+// Oversættelse
+import { LocalizeProvider } from 'react-localize-redux';
+
+// Components
+import LocalizedApp from './App';
+import LoadingPage from './components/Misc/Utility-pages/LoadingPage';
+
+// STYLING
+import './styles/scss/main.scss';
+// Lightbox css
+import 'react-image-lightbox/style.css'; // This only needs to be imported once in your app
+
+// redux
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+
+const migrations: any = {
+  10: (state: IReduxState) => ({
+    ...state,
+    questions: null
+  })
+};
+
+const persistConfig = {
+  key: 'medMCQ',
+  storage: storage,
+  stateReconciler: autoMergeLevel2, // see "Merge Process" section for details.
+  version: 10,
+  migrate: createMigrate(migrations),
+  whitelist: ['quiz', 'questions', 'metadata', 'ui', 'settings', 'shareBuilder'] // to disable persists
+};
+
+const pReducer = persistReducer(persistConfig, reducers);
+
+// Redux middleware
+let middleware = getDefaultMiddleware();
+/**
+ * removes createSerializableStateInvariantMiddleware which threw a bunch of errs
+ * over react-localize-redux and redux-persist.
+ */
+if (middleware.length > 1) middleware.pop();
+
+export const store = configureStore({
+  reducer: pReducer,
+  middleware,
+  devTools: { maxAge: 20 }
+});
+
+export const persistor = persistStore(store);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={<LoadingPage />} persistor={persistor}>
+      <ApolloProvider client={apolloClient}>
+        <LocalizeProvider store={store}>
+          <LocalizedApp />
+        </LocalizeProvider>
+      </ApolloProvider>
+    </PersistGate>
+  </Provider>,
+  document.querySelector('#root')
+);
+
+serviceWorker.unregister();
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,77 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
-// GraphQL
-import { ApolloProvider } from 'react-apollo-hooks';
-import apolloClient from 'apolloClient';
-// Redux
-import { Provider } from 'react-redux';
-import { createMigrate, persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/es/storage';
-import autoMergeLevel2 from 'redux-persist/es/stateReconciler/autoMergeLevel2';
-import { PersistGate } from 'redux-persist/lib/integration/react';
-import reducers, { IReduxState } from './reducers';
+import IE11 from 'IE11/IE11';
 
-// Oversættelse
-import { LocalizeProvider } from 'react-localize-redux';
-
-// Components
-import LocalizedApp from './App';
-import LoadingPage from './components/Misc/Utility-pages/LoadingPage';
-
-// STYLING
-import './styles/scss/main.scss';
-// Lightbox css
-import 'react-image-lightbox/style.css'; // This only needs to be imported once in your app
-
-// redux
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-
-const migrations: any = {
-  10: (state: IReduxState) => ({
-    ...state,
-    questions: null
-  })
-};
-
-const persistConfig = {
-  key: 'medMCQ',
-  storage: storage,
-  stateReconciler: autoMergeLevel2, // see "Merge Process" section for details.
-  version: 10,
-  migrate: createMigrate(migrations),
-  whitelist: ['quiz', 'questions', 'metadata', 'ui', 'settings', 'shareBuilder'] // to disable persists
-};
-
-const pReducer = persistReducer(persistConfig, reducers);
-
-// Redux middleware
-let middleware = getDefaultMiddleware();
 /**
- * removes createSerializableStateInvariantMiddleware which threw a bunch of errs
- * over react-localize-redux and redux-persist.
+ * Handle errormessage on internet explorer
  */
-if (middleware.length > 1) middleware.pop();
-
-export const store = configureStore({
-  reducer: pReducer,
-  middleware,
-  devTools: { maxAge: 20 }
-});
-
-export const persistor = persistStore(store);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <PersistGate loading={<LoadingPage />} persistor={persistor}>
-      <ApolloProvider client={apolloClient}>
-        <LocalizeProvider store={store}>
-          <LocalizedApp />
-        </LocalizeProvider>
-      </ApolloProvider>
-    </PersistGate>
-  </Provider>,
-  document.querySelector('#root')
-);
-
-serviceWorker.unregister();
+if (window.navigator.userAgent.match(/(MSIE|Trident)/)) {
+  ReactDOM.render(<IE11 />, document.querySelector('#root'));
+} else {
+  require('./IndexApp');
+}
