ca9215cae5d34d5ae5bcf7cb82be78d8b3ea492a
Thomas Jensen <thomasjensen1194@gmail.com>
2020-02-06 14:34:11 +0100

    Fixed login component errors

--- client/src/classes/Apollo.ts
+++ client/src/classes/Apollo.ts
@@ -1,34 +1,22 @@
 import client from '../apolloClient';
-import { store } from 'IndexApp';
-import makeToast from 'redux/actions/makeToast';
 
 class Apollo {
   static query = async <A>(name: string, query: any, variables?: { [key: string]: any }) => {
-    try {
-      const res = await client.query({
-        query,
-        variables
-      });
+    const res = await client.query({
+      query,
+      variables
+    });
 
-      return res.data[name] as A;
-    } catch (error) {
-      console.error(error);
-      store.dispatch(makeToast('toast.genericError', 'error'));
-    }
+    return res.data[name] as A;
   };
 
   static mutate = async <A>(name: string, mutation: any, variables?: { [key: string]: any }) => {
-    try {
-      const res = await client.mutate({
-        mutation,
-        variables
-      });
+    const res = await client.mutate({
+      mutation,
+      variables
+    });
 
-      return res.data[name] as A;
-    } catch (error) {
-      console.error(error);
-      store.dispatch(makeToast('toast.genericError', 'error'));
-    }
+    return res.data[name] as A;
   };
 }
 
