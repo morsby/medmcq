fcb28338b5bde6b3ac79d1f9b758a46e54cd0d67
Thomas Jensen <44089656+thomasjensen1194@users.noreply.github.com>
2020-03-05 19:17:54 +0100

    Updated ApolloClient

--- client/src/apolloClient.ts
+++ client/src/apolloClient.ts
@@ -1,14 +1,30 @@
-import ApolloClient from 'apollo-boost';
+import ApolloClient from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
+import { RetryLink } from 'apollo-link-retry';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const link = ApolloLink.from([
+  new RetryLink(),
+  new ApolloLink((operation, forward) => {
+    return forward(operation).map((data) => {
+      if (data && data.errors && data.errors.length > 0) {
+        throw new Error('GraphQL Operational Error');
+      }
+      return data;
+    });
+  }),
+  new HttpLink({ uri: '/graphql' })
+]);
 
 const client = new ApolloClient({
-  uri: '/graphql',
-  credentials: 'include'
-});
-
-client.defaultOptions = {
-  query: {
-    fetchPolicy: 'network-only'
+  link,
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    query: {
+      fetchPolicy: 'network-only'
+    }
   }
-};
+});
 
 export default client;
