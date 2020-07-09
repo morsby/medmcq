cd39585d1c45b72b6266f18c1ecf030f2bc122ae
Thomas Jensen <44089656+thomasjensen1194@users.noreply.github.com>
2020-07-09 16:28:34 +0200

    Disabled cypress video recording

--- server/migrations/20200709152254_add_semester_to_notifications.ts
+++ server/migrations/20200709152254_add_semester_to_notifications.ts
@@ -12,7 +12,7 @@ export async function up(knex: Knex): Promise<any> {
   const notifications = await knex('notifications');
   let i = 0;
   for (let n of notifications) {
-    console.log('Parsing notification ' + i + ' off ' + notifications.length);
+    console.log('Parsing notification ' + i + ' of ' + notifications.length);
     const capture = n.message.split(/https:\/\/medmcq.au.dk\/quiz\/(\d+)/);
     const questionId = Number(capture[1]);
     if (Number.isNaN(questionId)) {
