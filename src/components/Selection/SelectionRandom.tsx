20ecc4b2192c5b232a8eb73a914a5daf7f76f2c8
Thomas Jensen <44089656+thomasjensen1194@users.noreply.github.com>
2021-01-13 13:05:48 +0100

    Added search for editor users

--- src/components/Selection/SelectionRandom.tsx
+++ src/components/Selection/SelectionRandom.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import SelectionNSelector from './SelectionComponents/SelectionNSelector';
+import { Divider } from 'semantic-ui-react';
+import SelectionSearch from './SelectionComponents/SelectionSearch';
+import { useSelector } from 'react-redux';
+import { ReduxState } from 'redux/reducers';
 
 export interface SelectionRandomProps {}
 
-const SelectionRandom: React.SFC<SelectionRandomProps> = () => {
+const SelectionRandom: React.FC<SelectionRandomProps> = () => {
+  const user = useSelector((state: ReduxState) => state.auth.user);
+
   return (
     <div>
       <SelectionNSelector />
+      {user?.role.id <= 3 && (
+        <>
+          <Divider />
+          <SelectionSearch />
+        </>
+      )}
     </div>
   );
 };
