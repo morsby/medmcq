b86ccce7e80451a9e95cb5ced4ab5fc9c89d79d7
Thomas Jensen <44089656+thomasjensen1194@users.noreply.github.com>
2021-01-25 19:23:41 +0100

    Update contactTranslations.ts

--- src/components/Misc/Utility/contactTranslations.ts
+++ src/components/Misc/Utility/contactTranslations.ts
@@ -5,11 +5,11 @@ export default {
       `
 Når du sender din besked, bliver den offentligt tilgængelig. Der oprettes en ny tråd på https://github.com/thjendk/medmcq/issues.
 
-Vi har ikke mulighed for at kontakte dig direkte som svar på din henvendelse, så har du behov for opfølgning henvises til ovenstående link.`,
+Vi har ikke mulighed for at kontakte dig direkte som svar på din henvendelse, så har du behov for opfølgning henvises til ovenstående link. Drejer din henvendelse sig om personlige problemer, såsom problemer med din bruger, eller ønsker du at vi kan svare dig tilbage, så kan du kontakte os på medmcqau@gmail.com.`,
       `
 When you send your message, it will become public. A new thread will be created at https://github.com/thjendk/medmcq/issues.
 
-We cannot contact you directly. If you want to follow up on your message, please visit the link above.`
+We cannot contact you directly. If you want to follow up on your message, please visit the link above. Should you require personal assistance, for example with user issues, or do you want to be able to get an answer through email, then please contact us using medmcqau@gmail.com.`
     ],
     form: {
       subject: ['Overskrift', 'Subject'],
